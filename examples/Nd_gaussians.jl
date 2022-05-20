75d856704b563873adf85c2c09e06dff97d70908
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2022-05-20 18:24:25 +0200

    Implement proper 1D functionality for metropolis hastings (#30)

--- examples/Nd_gaussians.jl
+++ examples/Nd_gaussians.jl
@@ -1,25 +1,23 @@
 using Distributed, StatsBase
 
-addprocs(4, exeflags="--project")
+addprocs(4; exeflags="--project")
 @everywhere begin
-
     using TransitionalMCMC, Distributions, LinearAlgebra
     Ndims = 5
     # Prior Bounds
-    lb, ub  = -10, 10
+    lb, ub = -10, 10
 
     mean1 = ones(Ndims) * 5
     mean2 = ones(Ndims) * (-5)
-    
-    cov = 1* Matrix(I, Ndims, Ndims)
+
+    cov = 1 * Matrix(I, Ndims, Ndims)
 
     # Prior Density and sampler
-    logprior(x) = sum(logpdf(Uniform.(lb, ub), x))
+    logprior(x) = sum(logpdf.(Uniform(lb, ub), x))
     priorRnd(Nsamples) = rand(Uniform(lb, ub), Nsamples, Ndims)
 
     # Log Likelihood
     logLik(x) = log.(pdf(MvNormal(mean1, cov), x) + pdf(MvNormal(mean2, cov), x))
-
 end
 
 Nsamples = 1000
